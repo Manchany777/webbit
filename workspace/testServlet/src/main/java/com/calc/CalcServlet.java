fe457d92a5e08c0a72a7ab364d6cbe31920a0a8c
Manchan777 <freshguni@gmail.com>
2023-08-30 01:06:33 +0900

    숙제

---
+++ workspace/testServlet/src/main/java/com/calc/CalcServlet.java
@@ -0,0 +1,47 @@
+package com.calc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcServlet
+ */
+//@WebServlet("/calc")
+public class CalcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public CalcServlet() {
+        super();
+
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 데이터 받기
+		int xvalue = Integer.parseInt(request.getParameter("x"));
+		int yvalue = Integer.parseInt(request.getParameter("y"));
+		
+		// 응답
+		response.setContentType("text/html;charset=UTF-8"); 
+		PrintWriter out = response.getWriter(); // 생성 - new 안써도 response객체의 getWriter()메소드에 의해 생성이 됨
+		out.println("<html>");
+		out.println("<body>");
+		out.println(xvalue + " + " + yvalue + " = " + (xvalue + yvalue) + "<br/><br/>");
+		out.println(xvalue + " - " + yvalue + " = " + (xvalue - yvalue) + "<br/><br/>");
+		out.println(xvalue + " * " + yvalue + " = " + (xvalue * yvalue) + "<br/><br/>");
+		out.println(xvalue + " / " + yvalue + " = " + (xvalue / yvalue) + "<br/><br/>");
+		out.println("<br/>");
+		out.println("<input type='button' value='뒤로가기' onclick='history.go(-1);' />");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
